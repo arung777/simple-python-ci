d7cd7472ff545361c86b35e0a23c1c5800eb60de
arung777 <arung777@users.noreply.github.com>
2025-03-17 06:26:57 +0000

    deploy: bdd5b86c51399f8a12145e79a3d8347c1c286cb7

---
+++ test_math_utils.py
@@ -0,0 +1,13 @@
+import unittest
+from maths_utils import add, multiply
+
+class TestMathUtils(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(2, 3), 6)
+if __name__ == "__main__":
+    unittest.main()
